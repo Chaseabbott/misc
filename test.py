9afdda4d60969ff2bb7abec15f67f6be7b7d863f
Chaser <Chaseabbott@users.noreply.github.com>
2017-03-29 10:10:39 -0700

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-var devcentral='https://twitter.com/devcentral';
-var amamos='Don\'t forget to follow us on'
-var communitymatters='You\'ll shoot your eye out'
-console.log(communitymatters)
-console.log(amamos,devcentral);
